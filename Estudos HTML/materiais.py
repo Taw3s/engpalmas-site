5178328b49a279d4220e403b585a5cadd8c95960
Davi Dantas <davichaves198@gmail.com>
2026-02-19 10:32:08 -0300

    Add files via upload

---
+++ Estudos HTML/materiais.py
@@ -0,0 +1,9 @@
+materiais = []
+
+for i in range(3):
+    material = input('Digite o nome do material:')
+    materiais.append(material)
+
+print('Os materiais são:')
+print(materiais)
+    
---
+++ Estudos HTML/setur.py
@@ -0,0 +1,3 @@
+setup = ["Processador i5", "Placa Gráfica RTX 3060", "RAM 8GB", "SSD 500GB"]
+setup[2] = 'RAM 16GB DDR5'
+print(setup)
